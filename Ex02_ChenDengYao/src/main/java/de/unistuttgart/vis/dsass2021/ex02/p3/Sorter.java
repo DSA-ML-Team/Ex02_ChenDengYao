639ff79002a17af319b97eb4ac3fd2f2b3cf757a
ad027yycn <58372516+ad027yycn@users.noreply.github.com>
2021-05-09 23:48:49 +0200

    Add files via upload

--- Ex02_ChenDengYao/src/main/java/de/unistuttgart/vis/dsass2021/ex02/p3/Sorter.java
+++ Ex02_ChenDengYao/src/main/java/de/unistuttgart/vis/dsass2021/ex02/p3/Sorter.java
@@ -4,51 +4,78 @@ public class Sorter {
 		
 	public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
 	       
-           for (int i=0 ; i< list.size()-1 ; i++) {
+           for (int i=0 ; i< list.size() ; i++) {
         	   
         	   int maxIndex = i;
-        	    T max = list.get(i);
+        	   
+        	   T max = list.get(i);
+   	    	
         	    
         	    for(int j= i+1; j<list.size(); j++) {
-        	        
-        	    	if (max.compareTo(list.get(j))>0) {
-        	    	   
+        	    	
+        	    	if (max.compareTo(list.get(j))<0) {
+        	    	    
+        	    		max = list.get(j) ;
+        	    		
         	    		maxIndex = j;
+        	    		
         	    	}
         	    }
+        	    list.swap(i, maxIndex);
           }
+           for (int k=0 ; k< list.size() ; k++) {
+              System.out.println(list.get(k));
+           }
+              System.out.println("\n");
+              
 	}
         	
 	
 	public static <T extends Comparable<T>> void insertionSort(ISimpleList<T> list) {
-		
+	
 		  for(int i=1; i<list.size()-1; i++){
-			
-			  for(int j=i; j>0; j--){
+			  
+			  T temp = list.get(i);
+			  int j=i;
+			  
+			  while(j>0){
 				
-				  T temp = list.get(j);
-				
-				  if(temp.compareTo(list.get(i))<0){
-					
-					  list.swap(i, j);
+				  if(temp.compareTo(list.get(j))>0){
+					  list.swap(j,j-1);
+					  j--;
 				    }
+				  else {break;}
 			     }
-
+               list.swap(i, j);
 	       }
-
+		  for (int k=0 ; k< list.size() ; k++) {
+		        System.out.println(list.get(k));
+		  }
+		        System.out.println("\n");
+		  
 	 }
 	
 	public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
 
-		for(int i=0; i<list.size()-2;i++) {
+		for(int i=0; i<list.size()-1;i++) {
 			
-			T temp = list.get(i);
+		  for(int j=0; j<list.size()-1;j++) {
 			
-			if (temp.compareTo(list.get(i+1))<0) {
+			T temp = list.get(j);
+			
+			if (temp.compareTo(list.get(j+1))<0) {
 				
-				list.swap(i, i+1);
+				list.swap(j, j+1);
 			}
 		}
+		}
+		for (int k=0 ; k< list.size() ; k++) {
+	        System.out.println(list.get(k));
+		}
+		    System.out.println("\n");
 	}
-
+	
+	
 }
+
+
--- Ex02_ChenDengYao/src/test/java/de/unistuttgart/vis/dsass2021/ex02/p3/SorterTest.java
+++ Ex02_ChenDengYao/src/test/java/de/unistuttgart/vis/dsass2021/ex02/p3/SorterTest.java
@@ -7,8 +7,67 @@ import org.junit.Test;
 import de.unistuttgart.vis.dsass2021.ex02.p3.ISimpleList;
 import de.unistuttgart.vis.dsass2021.ex02.p3.Sorter;
 
+
 public class SorterTest {
+	
+@Test	
+	public void testSorter1() {
+	
+	ISimpleList simList = new SimpleList();
+	simList.append(3);
+	simList.append(5);
+	simList.append(2);
+	simList.append(1);
+	simList.append(4);
+		Sorter.selectionSort(simList);
+		ISimpleList expList = new SimpleList();
+		expList.append(5);
+		expList.append(4);
+		expList.append(3);
+		expList.append(2);
+		expList.append(1);
+		
+		assertEquals(expList,simList);
+	}
 
-	// Add your test methods here
+@Test	
+public void testSorter2() {
 
+ISimpleList simList = new SimpleList();
+simList.append(3);
+simList.append(5);
+simList.append(2);
+simList.append(1);
+simList.append(4);
+	Sorter.insertionSort(simList);
+	ISimpleList expList = new SimpleList();
+	expList.append(5);
+	expList.append(4);
+	expList.append(3);
+	expList.append(2);
+	expList.append(1);
+	
+	assertEquals(expList,simList);
+}
+
+@Test	
+public void testSorter3() {
+
+ISimpleList simList = new SimpleList();
+simList.append(3);
+simList.append(5);
+simList.append(2);
+simList.append(1);
+simList.append(4);
+	Sorter.bubbleSort(simList);
+	ISimpleList expList = new SimpleList();
+	expList.append(1);
+	expList.append(2);
+	expList.append(3);
+	expList.append(4);
+	expList.append(5);
+	
+	assertEquals(expList,simList);
+}
+    
 }
